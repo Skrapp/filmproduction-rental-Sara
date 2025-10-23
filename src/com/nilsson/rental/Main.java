53b80a14d1be3bad60f3c6ba81ba2fd66f84c1f5
Sara Nilsson <nilssonsara97@gmail.com>
2025-10-23 10:30:54 +0200

    first commit

---
+++ src/com/nilsson/rental/Main.java
@@ -0,0 +1,24 @@
+package com.nilsson.rental;
+
+public class Main {
+
+/*Du har fått en provanställning på Wigellkoncernen och som första uppdrag ska du bygga
+en konsolbaserad Java-applikation som modellerar en medlemsklubb med uthyrning av
+utrustning. (t.ex. fordon, verktyg, sportprylar eller liknande)
+
+Klasser som ska finnas i applikationen:
+• Member (id, namn, status/level, historik)
+• Item (abstract)  Subklasser som ex. Vehicle, Tool, SportGear eller liknande
+beroende på ditt val av applikation. Var och en av klasserna ska innehålla sina
+unika attribut och metoder enligt det vi pratat om.
+• Rental (kopplar member och item för en viss tidsperiod)
+• PricePolicy (interface) + konkreta strategier, ex. standard, student, premium.
+• Inventory och MemberRegistry (hanteras i minnet via List, Map eller Set)
+• RentalService och MembershipService ska innehålla affärslogiken
+• Konsolmeny: lägg till/sök/ändra medlemmar. Lista/filtrera items. Boka/avsluta
+uthyrning. Summera intäkter
+*/
+    public static void main(String[] args) {
+
+    }
+}
